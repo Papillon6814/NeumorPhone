f6af754cf44c4ee7452618d30ec00b5e6e6a24c6
Papillon6814 <so1rou.k@gmail.com>
2020-02-19 17:33:56 +0900

    プロジェクト作成

---
+++ NeumorPhone/NeumorPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeumorPhone
+//
+//  Created by Papillon on 2020/02/19.
+//  Copyright © 2020 Papillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
